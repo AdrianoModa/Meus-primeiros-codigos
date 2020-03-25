07e48a7cb50d4696d99bc7043995b7babdab841a
Adriano Moda <adrianomodafeitosa@gmail.com>
2020-03-25 15:22:21 -0300

    ver se numero primo e correcao de logica contador

--- Algoritmos Faculdade/Quantidade de divisores.cpp
+++ Algoritmos Faculdade/Quantidade de divisores.cpp
@@ -5,18 +5,27 @@ using namespace std;
 
 int main(){
 
+	setlocale(LC_ALL,"Portuguese");
     int n,i,vdiv;
 
-    cout << "Informe um numero inteiro\n";
+    cout << "Informe um numero inteiro" << endl;
     cin >> n;
     i = 1;
-    vdiv = 1;
+    vdiv = 0;
     while(n >= i){
-        if (n % 2 == 0){
-            vdiv += 1;
-            i=i++;
+    	if(i == 1){
+    		cout << n << " é divido por: ";
+		}
+        if (n % i == 0){
+            cout << i << " ";
+            vdiv = vdiv + 1;
         }
+        i+=1;
     }
-    cout << vdiv << endl;
+    if(vdiv == 2){
+    	cout << "sendo assim o numero " << n << " é primo!";
+	}
+    
+    cout << "\nTotal de " << vdiv << " divisores" << endl;
 
 }
